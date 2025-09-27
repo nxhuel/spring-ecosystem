46d382a9db513ca1c5ef70a0ce2537c84dec6823
nxhuel <nahueltisera03@gmail.com>
2025-09-27 19:21:22 -0300

    articles: practica full-stack sin ayuda

--- src/main/java/com/nxhu/restfull_api/RestfullApiApplication.java
+++ src/main/java/com/nxhu/restfull_api/RestfullApiApplication.java
@@ -1,50 +1,12 @@
 package com.nxhu.restfull_api;
 
-import java.util.Arrays;
-import java.util.List;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.nxhu.restfull_api.local.infrastructure.adapters.output.persistence.entity.LocalEntity;
-import com.nxhu.restfull_api.local.infrastructure.adapters.output.persistence.repository.LocalRepository;
-import com.nxhu.restfull_api.product.infrastructure.adapters.output.persistence.entity.ProductEntity;
-import com.nxhu.restfull_api.product.infrastructure.adapters.output.persistence.repository.ProductRepository;
-
-import lombok.RequiredArgsConstructor;
-
 @SpringBootApplication
-@RequiredArgsConstructor
-public class RestfullApiApplication implements CommandLineRunner {
-	
-	private final LocalRepository localRepository;
-	private final ProductRepository productRepository;
+public class RestfullApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestfullApiApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		List<LocalEntity> localEntities = Arrays.asList(
-				new LocalEntity(null, "Local 1", "Piso 1"),
-				new LocalEntity(null, "Local 2", "Piso 2"),
-				new LocalEntity(null, "Local 3", "Piso 3"),
-				new LocalEntity(null, "Local 4", "Piso 4"),
-				new LocalEntity(null, "Local 5", "Piso 5")
-				);
-		
-		localRepository.saveAll(localEntities);
-		
-		List<ProductEntity> productEntities = Arrays.asList(
-				new ProductEntity(null, "product 1", 100.0),
-				new ProductEntity(null, "product 2", 200.0),
-				new ProductEntity(null, "product 3", 300.0),
-				new ProductEntity(null, "product 4", 400.0),
-				new ProductEntity(null, "product 5", 500.0)
-				);
-				
-		productRepository.saveAll(productEntities);
 	}
 }
