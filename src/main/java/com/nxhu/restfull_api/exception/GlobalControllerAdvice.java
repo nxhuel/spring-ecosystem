5f733d8912647039aa8e32d0bcda5497141084c3
nxhuel <nahueltisera03@gmail.com>
2025-09-27 22:40:19 -0300

    article: arq en capas

---
+++ src/main/java/com/nxhu/restfull_api/exception/GlobalControllerAdvice.java
@@ -0,0 +1,50 @@
+package com.nxhu.restfull_api.exception;
+
+import java.net.http.HttpHeaders;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.nxhu.restfull_api.article.domain.exception.ArticleNotFoundException;
+import com.nxhu.restfull_api.article.infrastructure.adapters.in.rest.dto.response.ErrorResponse;
+
+@ControllerAdvice
+public class GlobalControllerAdvice extends ResponseEntityExceptionHandler {
+	
+//	slice article
+	@ExceptionHandler(ArticleNotFoundException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public ResponseEntity<ErrorResponse> userNotFoundException(ArticleNotFoundException exception) {
+		ErrorResponse message = ErrorResponse.builder()
+				.status(HttpStatus.NOT_FOUND)
+				.message(exception.getMessage())
+				.details(List.of())
+				.timestamp(LocalDateTime.now())
+				.build();
+		
+		return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
+	}
+
+
+//	global
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+		List<String> details = ex.getBindingResult().getFieldErrors().stream()
+				.map(error -> error.getField() + ": " + error.getDefaultMessage()).toList();
+
+		ErrorResponse errorResponse = ErrorResponse.builder().status(HttpStatus.BAD_REQUEST)
+				.message("Validation failed").details(details).timestamp(LocalDateTime.now()).build();
+
+		return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+	}
+}
